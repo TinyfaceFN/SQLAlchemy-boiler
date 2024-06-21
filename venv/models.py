4b5dcee17a2da73ffdfd953bbb211f3d28811301
Austin hughes <austinhughes@Austins-MBP.lan>
2024-06-21 18:09:49 -0400

    add

--- venv/models.py
+++ venv/models.py
@@ -4,8 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 #calls logic to connect to adatabase  
+
+# models here 
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer(), primary_key = True , autoincrement = True)
+
+    first_name = db.Column(db.Text())
+
+    last_name = db.Column(db.Text())
+
+    image_url = db.Column(db.Text(),default='https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg')
+
+
+
+
 def connect_db(app):
     db.app = app
     db.init_app(app)
-
-# models here 
